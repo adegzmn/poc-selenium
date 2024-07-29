c09823c833920bf3e64e948fa851fcd50b1b6d9d
angelawrites <angela@iconicbooknotes.com>
2024-07-29 12:43:14 +0800

    Added sysout loggers

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -38,37 +38,43 @@ public class Contact_Us_Steps {
     @Given("I access the WebDriver University - Contact Us Page")
     public void i_access_the_web_driver_university_contact_us_page() {
         driver.get("http://webdriveruniversity.com/Contact-Us/contactus.html");
+        System.out.println("I access the WebDriver University - Contact Us Page");
     }
 
     @When("I enter a unique first name")
     public void i_enter_a_unique_first_name() {
         driver.findElement(By.cssSelector("input[placeholder='First Name']")).sendKeys("Auto FN"+generateRandomString(3));
+        System.out.println("I enter a unique first name");
     }
 
     @When("I enter a unique last name")
     public void i_enter_a_unique_last_name() {
         driver.findElement(By.cssSelector("input[placeholder='Last Name']")).sendKeys("Auto LN"+generateRandomString(3));
+        System.out.println("I enter a unique last name");
     }
 
     @When("I enter a unique email address")
     public void i_enter_a_unique_email_address() {
         driver.findElement(By.cssSelector("input[placeholder='Email Address']")).sendKeys("AutoMail"+generateRandomString(10)+"@mail.com");
+        System.out.println("I enter a unique email address");
     }
 
     @When("I enter a unique comment")
     public void i_enter_a_unique_comment() {
         driver.findElement(By.cssSelector("textarea[placeholder='Comments']")).sendKeys("Auto Comment"+generateRandomString(3));
+        System.out.println("I enter a unique comment");
     }
 
     @When("I click on the submit button")
     public void i_click_on_the_submit_button() {
         driver.findElement(By.cssSelector("input[value='SUBMIT']")).click();
+        System.out.println("I click on the submit button");
     }
 
     @Then("I should be presented with a successful contact us submission message")
     public void i_should_be_presented_with_a_successful_contact_us_submission_message() {
         WebElement message = driver.findElement(By.cssSelector("#contact_reply h1"));
         Assert.assertEquals(message.getText(), "Thank You for your Message!");
-        System.out.println("SUCCESS");
+        System.out.println("I should be presented with a successful contact us submission message");
     }
 }
