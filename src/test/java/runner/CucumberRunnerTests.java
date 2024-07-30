0ac1ee11ac20a0ad6ed94e821039c08fee77da95
angelawrites <angela@iconicbooknotes.com>
2024-07-30 10:21:13 +0800

    Added extent reports

--- src/test/java/runner/CucumberRunnerTests.java
+++ src/test/java/runner/CucumberRunnerTests.java
@@ -1,11 +1,14 @@
 package runner;
 
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerAdapter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Listeners;
 
 @CucumberOptions(tags = "", features = {"src/test/resources/features/Contact_Us.feature"}, glue = {"stepDefinitions"},
         plugin = {})
 
+@Listeners({ExtentITestListenerAdapter.class})
 public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
 
 }
